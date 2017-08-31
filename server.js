94158ba0a46677eb4a5f0a5b018c9cc1fa9cc2dc
Giuseppe Fragalà <gmfdev2000@hotmail.com>
2017-08-31 02:09:25 +0200

    Project in beta version.

--- server.js
+++ server.js
@@ -4,26 +4,22 @@ var path = require('path');
 var moment = require('moment');
 var bodyParser = require('body-parser')
 
+var multer  = require('multer')
+var upload = multer()
+
 app.get('/', function(req, res) {
       res.sendFile(path.join(__dirname + '/index.htm'));
 });
 
 
-app.get('/:id', function(req, res) {
-
-    res.send(output);
-});
-
-
-// Tell express to use the body-parser middleware and to not parse extended bodies
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// Route that receives a POST request to /
-app.post('/', function (req, res) {
-  const body = req.body
-  res.set('Content-Type', 'text/plain')
-  res.send(`You sent: ${body} to Express`)
+app.post('/get-file-size', upload.single('file'), function (req, res, next) {
+  // req.file is the `avatar` file 
+  // req.body will hold the text fields, if there were any 
+  if(!req.file)res.end('Internal Server Error')
+  obj={size: req.file.size}
+  res.end(JSON.stringify(obj));
 })
+
 
 
 app.listen(process.env.PORT || 3000)
